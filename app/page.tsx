98bc9ff926723dd1bf3f9d42dc0752536ae433b6
Zain Gulbaz <zaingulbaz8@gmail.com>
2024-10-25 15:25:46 +0500

    demo page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import HeroSection from "@/components/homepage/hero";
 
 export default function Home() {
   return (
-    <PageWrapper>
       <div className="flex flex-col max-w-[70rem]">
         <HeroSection />
       </div>
-    </PageWrapper>
   );
 }
--- components/homepage/hero.tsx
+++ components/homepage/hero.tsx
@@ -7,48 +7,16 @@ import { Button } from "../ui/button";
 
 export default function HeroSection() {
     return (
-        <div className='flex flex-col items-center justify-center mt-[7rem] mb-[3rem] p-3'>
+        <div className='flex flex-col items-center justify-center mt-4' style={{width:"99vw"}}>
             <h1 className="scroll-m-20 text-3xl sm:text-3xl md:text-5xl font-semibold tracking-tight lg:text-5xl text-center max-w-[550px]">
-                Launch your blog with Zain in just a few clicks
+                DEMO SUB DOMAINS
             </h1>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-lg text-sm text-center mt-2 dark:text-gray-400">
-                An opensource blog CMS built using Nextjs, Supabase & TipTap
-            </p>
             <div className="flex gap-3">
                 <Link href="/cms" className="mt-5">
-                    <Button size="sm" className="animate-buttonheartbeat rounded-md bg-blue-600 hover:bg-blue-400 text-sm font-semibold text-white">Dashboard</Button>
+                    <Button size="sm" className="animate-buttonheartbeat rounded-md bg-blue-600 hover:bg-blue-400 text-sm font-semibold text-white">GO TO Dashboard</Button>
                 </Link>
-                <Link href="https://www.youtube.com/watch?v=rUD3OYo6ziM" target='_blank' className="mt-5">
-                    <Button size="sm" variant="ghost" className="flex gap-1 text-blue-600 hover:text-blue-600 hover:bg-blue-100">YouTube Video<ArrowRight className='w-4 h-4' /></Button>
-                </Link>
+            
             </div>
-            <div>
-                <div className="relative flex max-w-4xl justify-center overflow-hidden">
-                    <motion.div
-                        initial={{ y: -100, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        transition={{ type: 'spring', stiffness: 100, damping: 3 }}
-                        className="relative flex max-w-4xl justify-center overflow-hidden"
-                    >
-                        <div className="relative flex max-w-4xl justify-center overflow-hidden mt-7">
-                            <div className="relative rounded-xl">
-                                <img
-                                    src="/home.png"
-                                    alt="Hero Image"
-                                    className="block dark:hidden w-[1400px] rounded-[inherit] border object-contain shadow-lg"
-                                />
-                                <img
-                                    src="/dash-dark.png"
-                                    alt="Hero Image"
-                                    className="hidden dark:block w-[1400px] rounded-[inherit] border object-contain shadow-lg"
-                                />
-                                <BorderBeam size={250} duration={12} delay={9} />
-                            </div>
-                        </div>
-                    </motion.div>
-                </div>
-            </div>
-
         </div>
     )
 }
